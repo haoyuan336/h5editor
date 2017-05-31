eee7a72ef014fc51518dc3f229869073eb371bf3
楚浩远 <328387458@qq.com>
2017-05-31 09:05:08 +0800

    h5editor_macos

---
+++ h5editor_macos/h5editor_macos/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  h5editor_macos
+//
+//  Created by  楚浩远 on 2017/5/31.
+//  Copyright © 2017年  楚浩远. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
