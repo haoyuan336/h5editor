86db22177fee56ed6d8c6e2159c5e72f1958b362
楚浩远 <328387458@qq.com>
2017-06-08 16:18:56 +0800

    none

--- h5editor_macos/h5editor_macos/ViewController.swift
+++ h5editor_macos/h5editor_macos/ViewController.swift
@@ -7,43 +7,17 @@
 //
 
 import Cocoa
-let on = 1//开
-let off = 0//关
+
 class ViewController: NSViewController,NSTextFieldDelegate {
-
-
-    
-    @IBOutlet weak var inputMessage: NSTextField!
-    @IBOutlet weak var collectionView: NSCollectionView!
-    let reuse = "reuseItem"
-    var data:[String] = ["张三","李四","王五","小六"]
-    var item = SBCollectionItem();
+    var airports = ["YYZ": "Toronto Pearson","DUB": "Dublin"];
     override func viewDidLoad() {
         super.viewDidLoad()
-        inputMessage.delegate = self
-        collectionView.register(forDraggedTypes: [reuse])
-        inputMessage.becomeFirstResponder()
-        
-//        inputMessage.becomeFirstResponder();
-        // Do any additional setup after loading the view.
-
-    }
-
-    @IBAction func add(_ sender: Any) {
-        addMessage();
-    }
-    func addMessage(){
-        if !inputMessage.stringValue.isEmpty{
-//            data.append(inputMessage.stringValue)
-            let indexPath = NSIndexPath.init(forItem: data.count - 1,inSection: 0)
-//            collectionView.insertItems(at: [indexPath as IndexPath])
-//            collectionView.scrollToItems(at: [indexPath as IndexPath], scrollPosition: .bottom)
-//            collectionView.reloadItems(at: [indexPath as IndexPath])
-            collectionView.insertItems(at: [indexPath as IndexPath])
-            inputMessage.stringValue = ""
-            
+        for index in 1...5 {
+            print("inde \(index) = \(index * 5)")
         }
     }
+
+  
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
