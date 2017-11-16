fad3c4abfbc77d3226c6f79820ecc583e00653f9
楚浩远 <328387458@qq.com>
2017-11-16 16:22:08 +0800

    none

--- test3/main.js
+++ test3/main.js
@@ -4,24 +4,17 @@ const url = require('url');
 const path = require('path');
 let mainWindow;
 app.on("ready", function(){
-    console.log('ready');
-    mainWindow = new BrowserWindow({width: 800, height: 800});
+   mainWindow = new BrowserWindow({
+       width: 800,
+       height: 800,
+       minWidth: 400,
+       minHeight: 400
+   });
     mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'mainWorld2.html'),
-        protocol: 'file',
+        pathname: path.join(__dirname, './view/index.html'),
+        protocol: "file:",
         slashes: true
     }));
 
-    // mainWindow.webContents.openDevTools()
-    let _isOpenDevtool = false;
-    ipcMain.on("open_dev_tool", function(){
-        console.log('open dev tool');
-        if (_isOpenDevtool){
-            _isOpenDevtool = false;
-            mainWindow.webContents.closeDevTools();    
-        }else{
-            mainWindow.webContents.openDevTools();        
-            _isOpenDevtool = true;
-        }
-    })
-})
+
+});
